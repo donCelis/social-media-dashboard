1720221bfba79c07e705e9a14a8379d1b0b260ce
Alexander Celis <dacelis0@misena.edu.co>
2020-10-09 14:19:39 -0500

    fixed

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   output: {
     path: paths.build,
     publicPath: '/social-dashboard-webpack/',
-    filename: './js/[name].[contenthash].bundle.js',
+    filename: 'js/[name].[contenthash].bundle.js',
   },
   plugins: [
     /**
@@ -23,7 +23,7 @@ module.exports = merge(common, {
      * They cannot be used together in the same config.
      */
     new MiniCssExtractPlugin({
-      filename: './styles/[name].[contenthash].css',
+      filename: 'styles/[name].[contenthash].css',
       /* chunkFilename: './styles/[id].css', */
     }),
   ],
